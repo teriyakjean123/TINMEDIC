370c318293333475e37dc669f4f58121be963b32
teriyakjean123 <somerandomemail@gmail.com>
2025-07-31 00:43:23 +0800

    Updated

--- src/app/[slug].tsx
+++ src/app/[slug].tsx
@@ -1,9 +1,16 @@
 import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { getPostBySlug } from "../repositories/postRepository";
+import { getPostBySlug, getAllPosts } from "../repositories/postRepository";
 import Markdown from 'react-native-markdown-display';
 
+export async function generateStaticParams(): Promise<Record<string, string>[]> {
+  const posts = getAllPosts();
+  // Return an array of params to generate static HTML files for.
+  // Each entry in the array will be a new page.
+  return posts.map(post => ({ slug: post.slug }));
+
+}
 const postDetailsPage = () => {
   const { slug } = useLocalSearchParams();
   const [post, setPost] = useState(getPostBySlug(slug as string));
